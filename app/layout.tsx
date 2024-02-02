7f1abc0597a4d00aa6f543254adf38cac5def572
luizgppires <luizgppires@gmail.com>
2024-02-02 19:32:11 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import 'pliny/search/algolia.css'
 
 import { Space_Grotesk } from 'next/font/google'
 import { AnalyticsConfig } from 'pliny/analytics'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
